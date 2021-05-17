1b026edcc2520975d8f97989d8def5550ca1a8fa
Raul <raul.hmetal@gmail.com>
2021-05-17 14:55:06 -0300

    Edit: Building connection methods on socket builder!

--- app/pages/_shared/socketBuilder.js
+++ app/pages/_shared/socketBuilder.js
@@ -1,17 +1,34 @@
+import { constants } from './constants.js';
+
 export default class SocketBuilder {
-    constructor({socketUrl}) {
-        this.socketUrl = socketUrl
+    constructor({ socketUrl, namespace }) {
+        this.socketUrl = socketUrl;
+        this.onUserConnected = () => undefined;
+        this.onUserDisconnected = () => undefined;
+    }
+
+    setOnUserConnected(fn) {
+        this.onUserConnected = fn;
+
+        return this;
+    }
+
+    setOnUserDisconnected(fn) {
+        this.onUserDisconnected = fn;
+
+        return this;
     }
 
     build() {
         const socket = globalThis.io.connect(this.socketUrl, {
-            withCredentials: false
-        })
+            withCredentials: false,
+        });
 
-        socket.on('connection', () => console.log('connection'))
-        socket.on('userConnected', () => console.log('connection'))
-        socket.on('userDisconnected', () => console.log('connection'))
+        socket.on('connection', () => console.log('connection'));
 
-        return socket
+        socket.on(constants.events.USER_CONNECTED, this.onUserConnected);
+        socket.on(constants.events.USER_DISCONNECTED, this.onUserDisconnected);
+
+        return socket;
     }
 }
--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -1,9 +1,9 @@
 const room = {
-  id: Date.now(),
-  topic: 'JS Expert é noix'
-}
+    id: Date.now(),
+    topic: 'JS Expert é noix',
+};
 
 const user = {
     img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/pilot_traveller_person_avatar-512.png',
-    username: 'Raul Rosá'
-}
+    username: 'Raul Rosá',
+};
