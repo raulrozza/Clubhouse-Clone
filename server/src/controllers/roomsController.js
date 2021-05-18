7f81155cc2476871fded1af4a26e7c09a502f50d
Raul <raul.hmetal@gmail.com>
2021-05-17 21:10:19 -0300

    Add: Receiving join room information

--- server/src/controllers/roomsController.js
+++ server/src/controllers/roomsController.js
@@ -1,21 +1,51 @@
+import Attendee from '../entities/attendee.js';
+
 export default class RoomsController {
-    constructor() {}
+    constructor() {
+        this._users = new Map();
+        this.rooms = new Map();
+    }
 
     onNewConnection(socket) {
         const { id } = socket;
         console.log(
             `${new Date().toLocaleTimeString()}: New connection by ${id}`,
         );
+        this._updateGlobalUserData({ userId: id });
     }
 
-    joinRoom(socket, data) {
-        console.log('Data received', data);
+    joinRoom(socket, { user, room }) {
+        const userId = (user.id = socket.id);
+        const roomId = room.id;
+
+        const updatedUserData = this._updateGlobalUserData({
+            userId,
+            userData: user,
+            roomId,
+        });
+    }
+
+    _updateGlobalUserData({ userId, userData = {}, roomId = '' }) {
+        const user = this._users.get(userId) ?? {};
+        const existingRoom = this.rooms.has(roomId);
+
+        const updatedUserData = new Attendee({
+            ...user,
+            ...userData,
+            roomId,
+            // If its the only person in the room
+            isSpeaker: !existingRoom,
+        });
+
+        this._users.set(userId, updatedUserData);
+
+        return this._users.get(userId);
     }
 
     getEvents() {
         const functions = Reflect.ownKeys(RoomsController.prototype)
             .filter(fn => fn !== 'constructor')
-            .map(name => this[name].bind(this));
+            .map(name => [name, this[name].bind(this)]);
 
         return new Map(functions);
     }
